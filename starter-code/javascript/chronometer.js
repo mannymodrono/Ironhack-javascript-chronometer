bae94a9826df2cd6b651bfc7a79540bafec91272
Manny Modrono <21243@columbushs.com>
2020-01-09 09:08:54 -0500

    did the first three iterations

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,8 +1,26 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime ++;
+
+    }, 1000)
+  }
+  getMinutes() {
+    let minutes = 0;
+    for (; this.currentTime >= 60;) {
+      minutes ++;
+      this.currentTime = this.currentTime - 60;
+    }
+    return minutes;
+  }
+  getSeconds() {
+    let seconds = this.currentTime;
+    return seconds;
+  }
   // twoDigitsNumber() {}
   // stopClick() {}
   // resetClick() {}
